0b64f27e8314f9f87f7448b4ae14e713e1b2bb63
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-10-17 17:49:40 -0400

    Update __init__.py

--- sillystring/__init__.py
+++ sillystring/__init__.py
@@ -3,10 +3,20 @@ import check50.c
 
 @check50.check()
 def exists():
-    """sillystring.c exists."""
+    """sillystring.c exists"""
     check50.exists("sillystring.c")
-    
+
 @check50.check(exists)
 def compiles():
-    """sillystring.c compiles."""
+    """sillystring.c compiles"""
     check50.c.compile("sillystring.c", lcs50=True)
+
+@check50.check(compiles)
+def converts_king_to_kInG():
+    """converts king to kInG"""
+    check50.run("./sillystring king").stdout("kInG").exit(0)
+
+@check50.check(compiles)
+def converts_King_to_KiNg():
+    """converts King to KiNg"""
+    check50.run("./sillystring King").stdout("KiNg").exit(0)
