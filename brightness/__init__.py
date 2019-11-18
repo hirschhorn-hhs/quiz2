24b3a260fa20b685e0b0573fa66dcfcca6d64482
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-11-18 16:22:30 -0500

    Update __init__.py

--- brightness/__init__.py
+++ brightness/__init__.py
@@ -10,3 +10,18 @@ def exists():
 def compiles():
     """brightness.c compiles"""
     check50.c.compile("brightness.c", lcs50=True)
+
+@check50.check(compiles)
+def rejects_0_args():
+    """rejects 0 arguments entered at the command line with exit code 1"""
+    check50.run("./brightness").exit(1)
+
+@check50.check(compiles)
+def rejects_2_args():
+    """rejects 2 arguments entered at the command line with exit code 1"""
+    check50.run("./brightness").exit(1)
+
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the score for "a" as 1"""
+    check50.run("./brightness beach.bmp").stdout("Brightness: 168", regex=False).exit(0)
