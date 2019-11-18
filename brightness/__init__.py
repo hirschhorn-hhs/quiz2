6ad36b7ff21cc471912772f0496b747d21800c83
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-11-18 16:26:05 -0500

    Update __init__.py

--- brightness/__init__.py
+++ brightness/__init__.py
@@ -23,5 +23,30 @@ def rejects_2_args():
 
 @check50.check(compiles)
 def computes_a_as_1():
-    """correctly calculates the score for "a" as 1"""
+    """correctly calculates the brightness for black.bmp as 0"""
+    check50.run("./brightness black.bmp").stdout("Brightness: 0", regex=False).exit(0)
+
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the brightness for blue.bmp as 85"""
+    check50.run("./brightness blue.bmp").stdout("Brightness: 85", regex=False).exit(0)
+
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the brightness for white.bmp as 255"""
+    check50.run("./brightness white.bmp").stdout("Brightness: 255", regex=False).exit(0)
+
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the brightness for beach.bmp as 168"""
     check50.run("./brightness beach.bmp").stdout("Brightness: 168", regex=False).exit(0)
+
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the brightness for dart.bmp as 134"""
+    check50.run("./brightness dart.bmp").stdout("Brightness: 134", regex=False).exit(0)
+    
+@check50.check(compiles)
+def computes_a_as_1():
+    """correctly calculates the brightness for night.bmp as 16"""
+    check50.run("./brightness night.bmp").stdout("Brightness: 16", regex=False).exit(0)
