9c63e857061a4c4d370e3e2a83f194615034a487
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-15 14:50:56 -0500

    Update __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -10,3 +10,8 @@ def exists():
 def compiles():
     """stack.c compiles"""
     check50.c.compile("stack.c", lcs50=True)
+
+    @check50.check(compiles)
+def pushes_and_prints_one_email():
+    """pushes and prints Hello, World email"""
+    check50.run("./stack").stdin("push").stdin("Hello, World").stdin("print").stdout("\n-> Printing all emails from newest to oldest.\nHello, World\nWhat would you like to do? (push, pop, print, quit) ")
