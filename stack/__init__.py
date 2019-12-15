8de5b76a82e5b1960de9f9e16d3e4fad3d173fc4
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-15 15:11:47 -0500

    Update __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -13,7 +13,7 @@ def compiles():
 
 @check50.check(compiles)
 def pushes_and_prints_one_email():
-    """pushes and prints Hello, World email"""
+    """pushes and prints email with Hello, World as subject"""
     from re import match
     expected = "-> Printing all emails from newest to oldest.\n\nHello, World"
     actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("print").stdout()
