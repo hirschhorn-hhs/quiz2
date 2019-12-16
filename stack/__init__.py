eedd0e6dcc9fb134e5b331cf6a2077b585a49863
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-16 16:38:48 -0500

    Update __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -42,7 +42,7 @@ def pushes_and_prints_one_email():
 def pushes_and_prints_three_emails():
     """pushes and prints emails with 'Hello, World', 'Are you there', and 'Seriously' as subjects"""
     from re import match
-    expected = "--> Printing all emails from newest to oldest.\n3\nAre you there\nHello, World\n"
+    expected = "--> Printing all emails from newest to oldest.\nSeriously\nAre you there\nHello, World\n"
     actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there").stdin("push").stdin("Seriously").stdin("print").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
@@ -62,5 +62,23 @@ def rejects_sixth_email():
     from re import match
     expected = "--> Printing all emails from newest to oldest.\n5\n4\n3\n2\n1\n"
     actual = check50.run("./stack").stdin("push").stdin("1").stdin("push").stdin("2").stdin("push").stdin("3").stdin("push").stdin("4").stdin("push").stdin("5").stdin("push").stdin("6").stdin("print").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def pushes_2_pops_1_and_prints():
+    """pushes 2 emails, pops 1 email, and then prints"""
+    from re import match
+    expected = "--> Printing all emails from newest to oldest.\nOhai\n"
+    actual = check50.run("./stack").("push").stdin("Ohai").stdin("push").stdin("Rly").stdin("pop").stdin("print").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def pushes_2_pops_1_pushes_1_and_prints():
+    """pushes 2 emails, pops 1 email, pushes another email, and then prints"""
+    from re import match
+    expected = "--> Printing all emails from newest to oldest.\nKbye\nOhai\n"
+    actual = check50.run("./stack").("push").stdin("Ohai").stdin("push").stdin("Rly").stdin("pop").stdin("push").stdin("Kbye").stdin("print").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
