6daa66321f92c12f395f58e6a5de5e6d5f1deea7
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-16 16:34:37 -0500

    Update __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -40,19 +40,19 @@ def pushes_and_prints_one_email():
  
 @check50.check(compiles)
 def pushes_and_prints_three_emails():
-    """pushes and prints emails with 'Hello, World', 'Are you there?', and 'Seriously!' as subjects"""
+    """pushes and prints emails with 'Hello, World', 'Are you there', and 'Seriously' as subjects"""
     from re import match
     expected = "--> Printing all emails from newest to oldest.\n3\nAre you there\nHello, World\n"
-    actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there").stdin("push").stdin("3").stdin("print").stdout()
+    actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there").stdin("push").stdin("Seriously").stdin("print").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
 
 @check50.check(compiles)
 def pushes_and_prints_five_emails():
-    """pushes and prints emails with 'Hello, World', 'Are you there?', 'Seriously!', 'C'mon...', and ':/' as subjects"""
+    """pushes and prints emails with 'Hello, World', 'Are you there', 'Seriously', 'C'mon', and 'Sad face' as subjects"""
     from re import match
-    expected = "--> Printing all emails from newest to oldest.\n:/\nC'mon...\nSeriously!\nAre you there?\nHello, World\n"
-    actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there?").stdin("push").stdin("Seriously!").stdin("push").stdin("C'mon...").stdin("push").stdin(":/").stdin("print").stdout()
+    expected = "--> Printing all emails from newest to oldest.\nSad face\nC'mon\nSeriously\nAre you there\nHello, World\n"
+    actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there").stdin("push").stdin("Seriously").stdin("push").stdin("C'mon").stdin("push").stdin("Sad face").stdin("print").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
 
