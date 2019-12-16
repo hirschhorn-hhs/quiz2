2a801faa0964eb7cf52ad888da65e395034675d3
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-16 16:05:49 -0500

    Update __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -13,9 +13,18 @@ def compiles():
 
 @check50.check(compiles)
 def pushes_and_prints_one_email():
-    """pushes and prints email with Hello, World as subject"""
+    """pushes and prints email with 'Hello, World' as subject"""
     from re import match
-    expected = "-> Printing all emails from newest to oldest.\n\nHello, World"
+    expected = "--> Printing all emails from newest to oldest.\nHello, World\n"
     actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("print").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+ 
+@check50.check(compiles)
+def pushes_and_prints_three_emails():
+    """pushes and prints emails with 'Hello, World', 'Are you there?', and 'Seriously!' as subjects"""
+    from re import match
+    expected = "--> Printing all emails from newest to oldest.\nHello, World\n"
+    actual = check50.run("./stack").stdin("push").stdin("Hello, World").stdin("push").stdin("Are you there?").stdin("push").stdin("Seriously!").stdin("print").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
