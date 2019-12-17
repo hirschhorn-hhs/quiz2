f3af346f04ee9cc45f6d9f27cff576740dc8e365
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2019-12-17 18:01:11 -0500

    Update __init__.py

--- queue/__init__.py
+++ queue/__init__.py
@@ -28,3 +28,48 @@ def raises_error_with_no_order_numbers_2():
     actual = check50.run("./queue").stdin("e").stdin("1").stdin("d").stdin("p").stdout()
     if not match(expected, actual):
         raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def enqueues_and_prints_one_order_number():
+    """enqueues and prints order number 1"""
+    from re import match
+    expected = "--> Printing all order numbers from oldest to newest.\norder #1\n"
+    actual = check50.run("./queue").stdin("e").stdin("1").stdin("p").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+ 
+@check50.check(compiles)
+def enqueues_and_prints_three_order_numbers():
+    """enqueues and prints order numbers 1, 2, and 3"""
+    from re import match
+    expected = "--> Printing all order numbers from oldest to newest.\nOrder #1\nOrder #2\nOrder #3\n"
+    actual = check50.run("./queue").stdin("e").stdin("1").stdin("e").stdin("2").stdin("e").stdin("3").stdin("p").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def enqueues_and_prints_fiver_order_numbers():
+    """enqueues and prints order numbers 1, 2, 3, 4, 5, and 6"""
+    from re import match
+    expected = "--> Printing all order numbers from oldest to newest.\nOrder #1\nOrder #2\nOrder #3\nOrder #4\nOrder #5\nOrder #6\n"
+    actual = check50.run("./queue").stdin("e").stdin("1").stdin("e").stdin("2").stdin("e").stdin("3").stdin("e").stdin("4").stdin("e").stdin("5").stdin("e").stdin("6").stdin("p").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def enqueues_2_dequeues_1_and_prints():
+    """enqueues 2 order numbers, dequeues 1 order number, and then prints"""
+    from re import match
+    expected = "--> Printing all order numbers from oldest to newest.\nOrder #2\n"
+    actual = check50.run("./queue").stdin("e").stdin("1").stdin("e").stdin("2").stdin("d").stdin("p").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
+
+@check50.check(compiles)
+def enqueues_2_dequeues_1_enqueues_1_and_prints():
+    """enqueues 2 order numbers, dequeues 1 order number, enqueues another order number, and then prints"""
+    from re import match
+    expected = "--> Printing all order numbers from oldest to newest.\nOrder #2\nOrder #3\n"
+    actual = check50.run("./queue").stdin("e").stdin("1").stdin("e").stdin("2").stdin("d").stdin("e").stdin("3").stdin("p").stdout()
+    if not match(expected, actual):
+        raise check50.Mismatch(expected, actual)
