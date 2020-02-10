c4eff7e34164870c1b65bcee41577a17c789b29b
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2020-02-10 15:54:28 -0500

    Update __init__.py

--- iowa/__init__.py
+++ iowa/__init__.py
@@ -29,3 +29,23 @@ def first_and_second_do_not_match():
 def interprets_b():
     """correctly interprets the results from County B with 10 county delegates and 1 state delegate"""
     check50.run("python3 iowa.py b.csv").stdin("10").stdin("1").stdout("SDEs for Buttigieg: 0.50\nSDEs for Sanders: 0.50").exit()
+
+@check50.check(exists)
+def interprets_c():
+    """correctly interprets the results from County C with 20 county delegates and 4 state delegates"""
+    check50.run("python3 iowa.py b.csv").stdin("20").stdin("4").stdout("SDEs for Biden: 0.80\nSDEs for Klobuchar: 1.60\nSDEs for Warren: 1.60").exit()
+
+@check50.check(exists)
+def interprets_d():
+    """correctly interprets the results from County D with 15 county delegates and 3 state delegates"""
+    check50.run("python3 iowa.py b.csv").stdin("15").stdin("3").stdout("SDEs for Buttigieg: 1.20\nSDEs for Sanders: 1.00\nSDEs for Warren: 0.80").exit()
+
+@check50.check(exists)
+def positive_int_county():
+    """only accepts a positive integer from the user for the number of total county delegates"""
+    check50.run("python3 iowa.py b.csv").stdin("-10").stdin("10").stdin("1").stdout("SDEs for Buttigieg: 0.50\nSDEs for Sanders: 0.50").exit()
+
+@check50.check(exists)
+def positive_int_state():
+    """only accepts a positive integer from the user for the number of total state delegates"""
+    check50.run("python3 iowa.py b.csv").stdin("10").stdin("0").stdin("1").stdout("SDEs for Buttigieg: 0.50\nSDEs for Sanders: 0.50").exit()
