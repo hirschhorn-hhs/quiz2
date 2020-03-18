8c2ea5589dcbc58f97cb8f2621fe6fb06b273409
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2020-03-18 15:26:49 -0400

    Update __init__.py

--- mail/__init__.py
+++ mail/__init__.py
@@ -10,3 +10,8 @@ def exists():
 def compiles():
     """mail.c compiles"""
     check50.c.compile("mail.c", lcs50=True)
+
+@check50.check(compiles)
+def cost_of_1_oz_envelope():
+    """calculates the cost of a 1 oz large envelope as $1.00"""
+    check50.run("./mail")..stdin("1").stdout("$1.00").exit(0)
