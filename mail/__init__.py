4e8b8f42524bb5706f186e639419fb3a1706b923
hirschhorn-hhs <54551703+hirschhorn-hhs@users.noreply.github.com>
2020-03-18 15:32:39 -0400

    Update __init__.py

--- mail/__init__.py
+++ mail/__init__.py
@@ -14,4 +14,24 @@ def compiles():
 @check50.check(compiles)
 def cost_of_1_oz_envelope():
     """calculates the cost of a 1 oz large envelope as $1.00"""
-    check50.run("./mail").stdin("1").stdout("$1.00").exit(0)
+    check50.run("./mail").stdin("1").stdout("$1.00\n").exit(0)
+
+@check50.check(compiles)
+def cost_of_2_point_5_oz_envelope():
+    """calculates the cost of a 2.5 oz large envelope as $1.40"""
+    check50.run("./mail").stdin("2.5").stdout("$1.40\n").exit(0)
+
+@check50.check(compiles)
+def cost_of_4_point_2_oz_envelope():
+    """calculates the cost of a 4.2 oz large envelope as $1.80"""
+    check50.run("./mail").stdin("4.2").stdout("$1.80\n").exit(0)
+
+@check50.check(compiles)
+def cost_of_12_point_34_oz_envelope():
+    """calculates the cost of a 12.34 oz large envelope as $3.40"""
+    check50.run("./mail").stdin("12.34").stdout("$3.40\n").exit(0)
+
+@check50.check(compiles)
+def cost_of_13_oz_envelope():
+    """calculates the cost of a 13 oz large envelope as $3.40"""
+    check50.run("./mail").stdin("13").stdout("$3.40\n").exit(0)
