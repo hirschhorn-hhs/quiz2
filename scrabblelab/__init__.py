f893236ac0fa603b966acf88caebac89b4f196de
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2020-11-12 09:16:06 -0500

    Update __init__.py

--- scrabblelab/__init__.py
+++ scrabblelab/__init__.py
@@ -10,3 +10,8 @@ def exists():
 def compiles():
     """scrabble.c compiles"""
     check50.c.compile("scrabble.c", lcs50=True)
+
+@check50.check(compiles)
+def 0():
+    """handles letter cases correctly"""
+    check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout("Tie!", regex=False).exit(0)
