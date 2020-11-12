f43b2499a836aa4cddd09e1ec4081f296fd431c3
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2020-11-12 09:20:46 -0500

    Update __init__.py

--- scrabblelab/__init__.py
+++ scrabblelab/__init__.py
@@ -15,3 +15,33 @@ def compiles():
 def 0():
     """handles letter cases correctly"""
     check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout("Tie!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 1():
+    """handles punctuation correctly"""
+    check50.run("./scrabble").stdin("Punctuation!?!?").stdin("punctuation").stdout("Tie!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 2():
+    """correctly identifies 'Question?' and 'Question!' as a tie"""
+    check50.run("./scrabble").stdin("Question?").stdin("Question!").stdout("Tie!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 3():
+    """correctly identifies 'hai!' as winner over 'Oh,'"""
+    check50.run("./scrabble").stdin("Oh,").stdin("hai!").stdout("Player 2 wins!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 4():
+    """correctly identifies 'COMPUTER' as winner over 'science'"""
+    check50.run("./scrabble").stdin("COMPUTER").stdin("science").stdout("Player 1 wins!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 5():
+    """correctly identifies 'Scrabble' as winner over 'wiNNeR'"""
+    check50.run("./scrabble").stdin("Scrabble").stdin("wiNNeR").stdout("Player 1 wins!", regex=False).exit(0)
+
+@check50.check(compiles)
+def 6():
+    """correctly identifies 'Skating!' as winner over 'figure?'"""
+    check50.run("./scrabble").stdin("figure?").stdin("Skating!").stdout("Player 2 wins!", regex=False).exit(0)
