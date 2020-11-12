73ed86bcb7c8167699384da82f319d27baacfa06
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2020-11-12 09:24:25 -0500

    Update __init__.py

--- scrabblelab/__init__.py
+++ scrabblelab/__init__.py
@@ -12,36 +12,36 @@ def compiles():
     check50.c.compile("scrabble.c", lcs50=True)
 
 @check50.check(compiles)
-def 0():
+def test0():
     """handles letter cases correctly"""
     check50.run("./scrabble").stdin("LETTERCASE").stdin("lettercase").stdout("Tie!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 1():
+def test1():
     """handles punctuation correctly"""
     check50.run("./scrabble").stdin("Punctuation!?!?").stdin("punctuation").stdout("Tie!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 2():
+def test2():
     """correctly identifies Question? and Question! as a tie"""
     check50.run("./scrabble").stdin("Question?").stdin("Question!").stdout("Tie!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 3():
+def test3():
     """correctly identifies hai! as winner over Oh,"""
     check50.run("./scrabble").stdin("Oh,").stdin("hai!").stdout("Player 2 wins!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 4():
+def test4():
     """correctly identifies COMPUTER as winner over science"""
     check50.run("./scrabble").stdin("COMPUTER").stdin("science").stdout("Player 1 wins!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 5():
+def test5():
     """correctly identifies Scrabble as winner over wiNNeR"""
     check50.run("./scrabble").stdin("Scrabble").stdin("wiNNeR").stdout("Player 1 wins!", regex=False).exit(0)
 
 @check50.check(compiles)
-def 6():
+def test6():
     """correctly identifies Skating! as winner over figure?"""
     check50.run("./scrabble").stdin("figure?").stdin("Skating!").stdout("Player 2 wins!", regex=False).exit(0)
