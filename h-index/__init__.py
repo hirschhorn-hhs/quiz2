88e3649c5f7f6b61a057a5520e69b134da1cf03e
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2020-12-07 16:31:40 -0500

    Update __init__.py

--- h-index/__init__.py
+++ h-index/__init__.py
@@ -15,3 +15,8 @@ def compiles():
 def reprompts_for_bad_input():
     """reprompts the user when an invalid number of papers is entered"""
     check50.run("./h-index").stdin("-1").stdin("0").stdin("21").reject()
+
+@check50.check(compiles)
+def h_index_0():
+    """correctly calcualtes an h-index of 0 for 1 paper with 0 citations"""
+    check50.run("./h-index").stdin("1").stdin("Paper 0").stdin("0").stdout("H-index: 0", regex=False).exit(0)
