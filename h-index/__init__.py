b1bb80ae3fcab98cfec5a31cae9713255fac8630
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2020-12-07 17:19:32 -0500

    Update __init__.py

--- h-index/__init__.py
+++ h-index/__init__.py
@@ -39,14 +39,14 @@ def h_index_3():
 @check50.check(compiles)
 def h_index_0_bonus():
     """[bonus] correctly sorts then calcualtes an h-index of 1 for 3 papers"""
-    check50.run("./h-index").stdin("10").stdin("Paper 1").stdin("1").stdin("Paper 2").stdin("1").stdin("Paper 3").stdin("20").stdout("H-index: 1", regex=False).exit(0)
+    check50.run("./h-index").stdin("3").stdin("Paper 1").stdin("1").stdin("Paper 2").stdin("1").stdin("Paper 3").stdin("20").stdout("H-index: 1", regex=False).exit(0)
 
 @check50.check(compiles)
 def h_index_1_bonus():
     """[bonus] correctly sorts then calcualtes an h-index of 2 for 3 papers"""
-    check50.run("./h-index").stdin("10").stdin("Paper 1").stdin("2").stdin("Paper 2").stdin("20").stdin("Paper 3").stdin("1").stdout("H-index: 2", regex=False).exit(0)
+    check50.run("./h-index").stdin("3").stdin("Paper 1").stdin("2").stdin("Paper 2").stdin("20").stdin("Paper 3").stdin("1").stdout("H-index: 2", regex=False).exit(0)
 
 @check50.check(compiles)
 def h_index_2_bonus():
     """[bonus] correctly sorts then calcualtes an h-index of 3 for 5 papers"""
-    check50.run("./h-index").stdin("10").stdin("Paper 1").stdin("2").stdin("Paper 2").stdin("20").stdin("Paper 3").stdin("1").stdin("Paper 4").stdin("5").stdin("Paper 5").stdin("10").stdout("H-index: 2", regex=False).exit(0)
+    check50.run("./h-index").stdin("5").stdin("Paper 1").stdin("2").stdin("Paper 2").stdin("20").stdin("Paper 3").stdin("1").stdin("Paper 4").stdin("5").stdin("Paper 5").stdin("10").stdout("H-index: 2", regex=False).exit(0)
