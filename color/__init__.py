47e7b24f71439e925d65f8ce2fc3f418cd3c0d16
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2022-10-15 18:18:10 -0400

    Update __init__.py

--- color/__init__.py
+++ color/__init__.py
@@ -12,6 +12,31 @@ def compiles():
     check50.c.compile("color.c", lcs50=True)
 
 @check50.check(compiles)
-def all_three_colors_match():
-    """all three colors match"""
+def all_three_colors_match_same_case():
+    """all three colors match when entered in the same way"""
     check50.run("./color").stdin("Pink").stdin("Pink").stdin("Pink").stdout("Everyone likes the same color!\n", regex=False).exit(0)
+
+@check50.check(compiles)
+def all_three_colors_match_diff_case():
+    """all three colors match when entered in different cases"""
+    check50.run("./color").stdin("Pink").stdin("pink").stdin("PINK").stdout("Everyone likes the same color!\n", regex=False).exit(0)
+
+@check50.check(compiles)
+def color1_and_color2_match():
+    """color1 and color2 match"""
+    check50.run("./color").stdin("Pink").stdin("pink").stdin("Red").stdout("Two people like the same color!\n", regex=False).exit(0)
+    
+@check50.check(compiles)
+def color1_and_color3_match():
+    """color1 and color3 match"""
+    check50.run("./color").stdin("Pink").stdin("Red").stdin("pink").stdout("Two people like the same color!\n", regex=False).exit(0)
+    
+@check50.check(compiles)
+def color2_and_color3_match():
+    """color2 and color3 match"""
+    check50.run("./color").stdin("Red").stdin("Pink").stdin("pink").stdout("Two people like the same color!\n", regex=False).exit(0)
+    
+@check50.check(compiles)
+def no_colors_match():
+    """no colors match"""
+    check50.run("./color").stdin("Pink").stdin("red").stdin("BLUE").stdout("No one likes the same color!\n", regex=False).exit(0)
