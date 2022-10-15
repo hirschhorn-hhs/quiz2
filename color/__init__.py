5c379931fb6963dcec531577e5486fe6fe8e8e3d
Jason Hirschhorn <3276647+hirschhorn@users.noreply.github.com>
2022-10-15 18:13:15 -0400

    Update __init__.py

--- color/__init__.py
+++ color/__init__.py
@@ -12,6 +12,6 @@ def compiles():
     check50.c.compile("color.c", lcs50=True)
 
 @check50.check(compiles)
-def all_three_colors_match:
+def all_three_colors_match():
     """all three colors match"""
     check50.run("./color").stdin("Pink").stdin("Pink").stdin("Pink").stdout("Everyone likes the same color!\n", regex=False).exit(0)
